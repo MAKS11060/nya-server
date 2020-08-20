2358e8c29cf3fb5299ffb01d809565ca537f9c94
maks11060 <maks11060@gmail.com>
2020-08-20 21:44:49 +0200

    upd

--- middleware/cookie.js
+++ middleware/cookie.js
@@ -1,91 +1,95 @@
-'use strict'
+import ms from 'ms'
 
-const ms = require('ms')
-// const crypto = require('crypto')
+const pairSplitRegExp = /; */
+//const sameSiteRegExp = /^(?:lax|strict)$/i
+//const fieldContentRegExp = /^[\u0009\u0020-\u007e\u0080-\u00ff]+$/
 
-const pairSplitRegExp = /; */;
-const sameSiteRegExp = /^(?:lax|strict)$/i
-const fieldContentRegExp = /^[\u0009\u0020-\u007e\u0080-\u00ff]+$/;
-
-const config = {
-	path: '/',
-	expires: undefined,
-	maxAge: 0,
-	domain: undefined,
-	httpOnly: true,
-	secure: true,
-	sameSite: false,
+const createCookie = prop => {
+	return `${prop.name}=${prop.value}`
+	+	(prop.domain   && `; domain=${prop.domain}` || '')
+	+ (prop.path     && `; path=${prop.path}` || '')
+	+ (prop.expires  && `; expires=${prop.expires.toUTCString()}` || '')
+	+ (prop.maxAge   && `; Max-Age=${typeof prop.maxAge === 'string' ? ms(prop.maxAge)/1000 : prop.maxAge}` || '')
+	+ (prop.httpOnly && '; httponly' || '')
+	+ (prop.secure   && '; secure' || '')
+	+ (prop.sameSite && `; samesite=${prop.sameSite === true ? 'strict' : prop.sameSite.toLowerCase()}` || '')
 }
 
-module.exports = ctx => {
-	const cookie = ctx.headers['cookie'] || null
-	const cookies = {}
-
-	Object.defineProperty(ctx, 'cookies', {
-		enumerable: false, writable: false, configurable: false,
-		value: cookie
-		? Object.fromEntries(cookie.split(pairSplitRegExp).map(i => i.split('=').map(i => i.trim())))
-		: {}
-	})
-
-	const createCookie = prop => {
-		let cookie = `${prop.name}=${prop.value}`
-
-		cookie += prop.domain
-		? `; domain=${prop.domain}` : ''
-		cookie += prop.path
-		? `; path=${prop.path}` : ''
-		cookie += prop.expires
-		? `; expires=${prop.expires.toUTCString()}` : ''
-		cookie += prop.maxAge 
-		? `; Max-Age=${prop.maxAge}` : ''
-		cookie += prop.httpOnly
-		? `; httponly` : ''
-		cookie += prop.secure
-		? `; secure` : ''
-		prop.sameSite
-		? `; samesite=${prop.sameSite === true ? 'strict' : prop.sameSite.toLowerCase()}` : ''
-
-		return cookie
+const writeCookie = ctx => {
+	const {cookies = {}} = ctx.state
+	ctx.header['Set-Cookie'] = []
+	for (const key in cookies) {
+		ctx.header['Set-Cookie'].push(cookies[key])
 	}
+}
 
-	const setCookies = () => {
-		ctx.header['Set-Cookie'] = []
-		for (const key in cookies) {
-			ctx.header['Set-Cookie'].push(cookies[key])
-		}
+export default config => {
+	config = {
+		domain: undefined,
+		path: '/',
+		expires: undefined,
+		maxAge: 0,
+		httpOnly: true,
+		secure: true,
+		sameSite: false,
+		...config
 	}
-
-	// methods
-	const set = (name, value, attr) => {
-		if (typeof attr === 'string') {attr={maxAge: ms(attr)/1000}}
-		if (typeof attr === 'number') {attr={maxAge: attr}}
-
-		cookies[name] = createCookie({
-			...config, name, value, ...attr
-		})
-
-		setCookies()
-	}
-
-	const get = name => ctx.cookies[name] || null
 	
-	const del = (...name) => {
-		for (const item of name.flat()) {
-			set(item, '', {expires: new Date(0)})
-		}
+	return ctx => {
+		const cookies = ctx.state.cookies = {}
+		const cookie = ctx.headers.cookie || ''
+
+		Object.defineProperties(ctx, {
+			cookies: {
+				get() {
+					return Object.fromEntries(
+						cookie.split(pairSplitRegExp)
+						.map(i => i.split('=').map(i => i.trim()))
+					)
+				}
+			},
+			cookie: {
+				enumerable: true,
+				value: Object.create(null, {
+					set: {
+						enumerable: true,
+						value: (name, value, attr) => {
+							if (typeof attr === 'string') {attr={maxAge: ms(attr)/1000}}
+							else if (typeof attr === 'number') {attr={maxAge: attr}}
+							
+							cookies[name] = createCookie({
+								...config, name, value, ...attr
+							})
+							
+							writeCookie(ctx)
+						}
+					},
+					get: {
+						enumerable: true,
+						value: name => ctx.cookies[name] || null
+					},
+					has: {
+						enumerable: true,
+						value: name => Boolean(ctx.cookies[name])
+					},
+					remove: {
+						enumerable: true,
+						value: (...name) => {
+							for (const item of name) {
+								ctx.cookie.set(item, '', {expires: new Date(0)})
+							}
+						}
+					},
+					clear: {
+						enumerable: true,
+						value: (...whitelist) => {
+							for (const [name] of Object.entries(ctx.cookies).filter(([key]) => !whitelist.includes(key))) {
+								ctx.cookie.remove(name)
+							}
+						}
+					}
+				})
+			}
+		})
 	}
-
-	const clear = () => {
-		for (let name in ctx.cookies) {
-			set(name, '', {expires: new Date(0)})
-		}
-	}
-
-	Object.defineProperty(ctx, 'cookie', {
-		enumerable: true, writable: false, value: {
-			set, get, del, clear
-		}
-	})
-
 }
