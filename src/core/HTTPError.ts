ec9efa0ff6944ba49adc9446c130194824616aa5
MAKS11060 <31521952+MAKS11060@users.noreply.github.com>
2022-10-18 03:02:00 +0200

    Update HTTPError.ts
    Add support json error message

--- src/core/HTTPError.ts
+++ src/core/HTTPError.ts
@@ -3,7 +3,8 @@ import {Status, StatusCode} from './types.js'
 export class HTTPError extends Error {
 	statusCode: number = 500
 
-	constructor(code: Status | number, message?: string) {
+	constructor(code: Status | number, message?: string | object, pretty?: boolean) {
+		if (typeof message == 'object') message = JSON.stringify(message, null, pretty ? '2' : undefined)
 		if (typeof code == 'string' && StatusCode[code]) {
 			super(message || code || 'Unknown')
 			this.statusCode = StatusCode[code]
