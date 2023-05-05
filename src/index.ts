f62d9c198c1a02607ced7d956401dc105c85c341
MAKS11060 <31521952+MAKS11060@users.noreply.github.com>
2023-05-05 23:21:26 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import 'urlpattern-polyfill'
+import {App} from './app.js'
+import {HttpError} from './httpError.js'
+import {Router} from './router/router.js'
 
 export {App} from './app.js'
 export {Server} from './server.js'
 export {Context} from './context.js'
-export {Router} from './router.js'
+export {Router} from './router/router.js'
+export {HttpError} from './httpError.js'
 
-export type {Handle, MiddlewareHandle} from './router.js'
+export type {RouteHandle, MiddlewareHandle} from './router/router.js'
