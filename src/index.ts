218a9d2665f8d8d783c14143a78c182a0e9e29d0
MAKS11060 <31521952+MAKS11060@users.noreply.github.com>
2023-06-04 07:37:16 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export {App} from './app.js'
 export {Server} from './server.js'
-export {Context} from './context.js'
 export {Router} from './router/router.js'
 export {HttpError} from './httpError.js'
 
+export type {Context} from './context.js'
 export type {RouteHandle, MiddlewareHandle} from './router/router.js'
